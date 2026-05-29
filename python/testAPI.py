7e06ff1d88f9b5f3261f4a12aa9172c1a63ca12a
YBTKS <bobby.tjassens.keiser@y-innovate-it.com>
2026-05-29 13:43:55 +0200

    test other system

--- python/testAPI.py
+++ python/testAPI.py
@@ -6,8 +6,8 @@ from getpass import getpass
 from Globals import Globals
 from MyRequests import MyRequests
 
-#Globals.myHost = "https://yinhdisv:8081"
-Globals.myHost = "https://mainframeyin:8092"
+Globals.myHost = "https://yinhdisv:8081"
+#Globals.myHost = "https://mainframeyin:8092"
 Globals.myBasepath = ""
 Globals.myCreds = ('YBTKS','')
 Globals.pathPrefix = "/petshop/API/v1"
@@ -26,6 +26,13 @@ print("Getting bearer token")
 MyRequests.getBearerToken(f"{Globals.pathPrefix}/token")
 
 def doStuff():
+    storeID = 0
+    supplierID = 0
+    productID = 0
+    inventoryID = 0
+    animalID = 0
+    padID = 0
+
     respget = MyRequests.get(f"{Globals.pathPrefix}/refTables/STORSTAT?tableKey=ACTIVE")
 
     if (respget.status_code == 200):
@@ -38,7 +45,7 @@ def doStuff():
             newRefTable["tableValue"] = "Store is active"
 
             X=input('touch')
-            resppost = MyRequests.post(f"{Globals.pathPrefix}/refTables?lww_debug=2", newRefTable)
+            resppost = MyRequests.post(f"{Globals.pathPrefix}/refTables", newRefTable)
 
             if (resppost.status_code != 201):
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
@@ -49,6 +56,8 @@ def doStuff():
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        storeID = respget.json()['stores'][0]['storeID']
     else:
         if (respget.status_code == 404):
             newStore = {}
@@ -58,7 +67,9 @@ def doStuff():
 
             resppost = MyRequests.post(f"{Globals.pathPrefix}/stores", newStore)
 
-            if (resppost.status_code != 201):
+            if (resppost.status_code == 201):
+                storeID = resppost.json()['storeID']
+            else:
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
         else:
             raise Exception(f'status code {str(respget.status_code)} {respget.text}')
@@ -67,6 +78,8 @@ def doStuff():
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        supplierID = respget.json()['suppliers'][0]['supplierID']
     else:
         if (respget.status_code == 404):
             newSupplier = {}
@@ -76,7 +89,9 @@ def doStuff():
 
             resppost = MyRequests.post(f"{Globals.pathPrefix}/suppliers", newSupplier)
 
-            if (resppost.status_code != 201):
+            if (resppost.status_code == 201):
+                supplierID = resppost.json()['supplierID']
+            else:
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
         else:
             raise Exception(f'status code {str(respget.status_code)} {respget.text}')
@@ -85,6 +100,8 @@ def doStuff():
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        productID = respget.json()['products'][0]['productID']
     else:
         if (respget.status_code == 404):
             newProduct = {}
@@ -92,42 +109,50 @@ def doStuff():
             newProduct["productStatus"] = "ACTIVE"
             newProduct["productName"] = "Deadbeef Dog Food"
             newProduct["groupCode"] = "PEDFCN01"
-            newProduct["supplierID"] = 41
+            newProduct["supplierID"] = supplierID
 
             resppost = MyRequests.post(f"{Globals.pathPrefix}/products", newProduct)
 
-            if (resppost.status_code != 201):
+            if (resppost.status_code == 201):
+                productID = resppost.json()['productID']
+            else:
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
         else:
             raise Exception(f'status code {str(respget.status_code)} {respget.text}')
 
-    respget = MyRequests.get(f"{Globals.pathPrefix}/inventory?storeID_filter=41")
+    respget = MyRequests.get(f"{Globals.pathPrefix}/inventory?storeID_filter={storeID}")
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        inventoryID = respget.json()['inventory'][0]['inventoryID']
     else:
         if (respget.status_code == 404):
             newInventory = {}
-            newInventory["storeID"] = 41
-            newInventory["productID"] = 3
+            newInventory["storeID"] = storeID
+            newInventory["productID"] = productID
             newInventory["sellByDate"] = "1900-01-01-00:00:00.000000"
             newInventory["inStock"] = 100
 
             resppost = MyRequests.post(f"{Globals.pathPrefix}/inventory", newInventory)
 
-            if (resppost.status_code != 201):
+            if (resppost.status_code == 201):
+                inventoryID = resppost.json()['inventoryID']
+            else:
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
         else:
             raise Exception(f'status code {str(respget.status_code)} {respget.text}')
     
-    respget = MyRequests.get(f"{Globals.pathPrefix}/animals?storeID_filter=41")
+    respget = MyRequests.get(f"{Globals.pathPrefix}/animals?storeID_filter={storeID}")
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        animalID = respget.json()['animalEntry'][0]['animalID']
     else:
         if (respget.status_code == 404):
             newAnimal = {}
-            newAnimal["storeID"] = 41
+            newAnimal["storeID"] = storeID
             newAnimal["animalType"] = "AT000001"
             newAnimal["animalRace"] = "AR000001"
             newAnimal["animalName"] = "Bob"
@@ -137,19 +162,40 @@ def doStuff():
 
             resppost = MyRequests.post(f"{Globals.pathPrefix}/animals", newAnimal)
 
-            if (resppost.status_code != 201):
+            if (resppost.status_code == 201):
+                animalID = resppost.json()['animalID']
+            else:
                 raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
         else:
             raise Exception(f'status code {str(respget.status_code)} {respget.text}')
 
-
-def doStuff2():
-    respget = MyRequests.get(f"{Globals.pathPrefix}/pricesAndDiscounts?lww_debug=2")
+    respget = MyRequests.get(f"{Globals.pathPrefix}/pricesAndDiscounts")
 
     if (respget.status_code == 200):
         print(f"{respget.status_code} {respget.text}")
+
+        padID = respget.json()['priceAndDiscount'][0]['padID']
     else:
-        raise Exception(f'status code {str(respget.status_code)} {respget.text}')
+        if (respget.status_code == 404):
+            newPAD = {}
+            newPAD['storeID'] = storeID
+            newPAD['productID'] = productID
+            newPAD['animalID'] = animalID
+            newPAD['price'] = 219.99
+            newPAD['discount'] = -10
+            newPAD['fromDate'] = "1900-01-01-00:00:00.000000"
+            newPAD['toDate'] = "1900-01-01-00:00:00.000000"
+
+            x=input('blub')
+
+            resppost = MyRequests.post(f"{Globals.pathPrefix}/pricesAndDiscounts?lww_debug=3", newPAD)
+
+            if (resppost.status_code == 201):
+                padID = resppost.json()['padID']
+            else:
+                raise Exception(f'status code {str(resppost.status_code)} {resppost.text}')
+        else:
+            raise Exception(f'status code {str(respget.status_code)} {respget.text}')
 
 #for i in range(0, 100):
-doStuff2()
+doStuff()
